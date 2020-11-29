3407ebd74d89c5017b66b459ea41ef5fa4e883d7
Nik Stires <nik.stires@gmail.com>
2020-11-28 18:31:58 -0800

    Added Page Animations

---
+++ src/pages/ContactUs.js
@@ -0,0 +1,20 @@
+import react from 'react';
+//Animations
+import { motion } from 'framer-motion';
+import { pageAnimation } from '../animation';
+
+const ContactUs = () => {
+  return (
+    <motion.div
+      exit='exit'
+      variants={pageAnimation}
+      initial='hidden'
+      animate='show'
+      style={{ background: '#fff' }}
+    >
+      <h1>Contact Us</h1>
+    </motion.div>
+  );
+};
+
+export default ContactUs;
